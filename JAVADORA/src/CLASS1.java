4ff012b12024acfb45ce03fba244578f482ae51b
kafeel <41783213+kafeelmit@users.noreply.github.com>
2019-08-08 23:58:07 +0430

    five

--- JAVADORA/src/CLASS1.java
+++ JAVADORA/src/CLASS1.java
@@ -4,7 +4,7 @@ public class CLASS1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("wael sohba online");
+		System.out.println("five");
 		
 		
 		
