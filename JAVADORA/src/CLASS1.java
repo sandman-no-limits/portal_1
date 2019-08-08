7f23ff2dbf53df4872c66fc4bdde9c26fcc2ed28
kafeel <41783213+kafeelmit@users.noreply.github.com>
2019-08-09 00:26:32 +0430

    merege method

--- JAVADORA/src/CLASS1.java
+++ JAVADORA/src/CLASS1.java
@@ -4,14 +4,21 @@ public class CLASS1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		meregr();
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	/**
+	 * 
+	 */
+	public static void meregr() {
 		System.out.println("ONE TOW THREE");
-		
-		
-		
-		
-		
-		
-		
 	}
 
 }
