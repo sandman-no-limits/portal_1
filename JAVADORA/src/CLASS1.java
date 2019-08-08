38c22218ae614194a5d261c4765fb2375f195169
kafeel <41783213+kafeelmit@users.noreply.github.com>
2019-08-09 00:25:28 +0430

    method rebase

--- JAVADORA/src/CLASS1.java
+++ JAVADORA/src/CLASS1.java
@@ -4,14 +4,21 @@ public class CLASS1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		m_rebase();
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	/**
+	 * 
+	 */
+	public static void m_rebase() {
 		System.out.println("REBASE");
-		
-		
-		
-		
-		
-		
-		
 	}
 
 }
